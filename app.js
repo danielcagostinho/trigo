3aefa965d3ea0dc0d852d1640725d6b51419bc1e
danielcagostinho <danielcagostinho@gmail.com>
2020-01-05 17:35:26 -0500

    Ready for Heroku

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.get("/breads", function(req, res){
+    res.render("breads");
+});
+
+app.get("/pastries", function(req, res){
+    res.render("pastries");
+})
+
+app.get("/cakes", function(req, res){
+    res.render("cakes");
+})
+
+app.listen(3000, function() {
+    console.log("Started Trigo Bakery");
+});
