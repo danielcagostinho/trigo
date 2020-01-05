06f520ce3d448e68368a862502de47f8eaf08ef4
danielcagostinho <danielcagostinho@gmail.com>
2020-01-05 18:33:26 -0500

    changed the dirname

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "../public"));
 
 app.get("/", function(req, res) {
     res.render("test");
