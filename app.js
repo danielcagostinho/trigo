eabbde4241c75cf254fe3dfe625d485a22f6cbe8
danielcagostinho <danielcagostinho@gmail.com>
2020-01-18 23:48:30 -0500

    Product Page test

--- app.js
+++ app.js
@@ -5,31 +5,26 @@ var app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.get("/", function(req, res){
+    res.render("test");
+});
+
 // app.get("/", function(req, res){
 //     res.render("index");
 // });
 
-app.get("/", function(req, res){
-    res.render("index");
+app.get("/breads", function(req, res){
+    res.render("breads");
 });
 
+app.get("/cakes", function(req, res){
+    res.render("cakes");
+});
 
-// app.get("/", function(req, res) {
-//     res.sendfile("release-1.html");
-// });
+app.get("/pastries", function(req, res){
+    res.render("pastries");
+});
 
-
-// app.get("/breads", function(req, res){
-//     res.render("breads");
-// });
-
-// app.get("/pastries", function(req, res){
-//     res.render("pastries");
-// })
-
-// app.get("/cakes", function(req, res){
-//     res.render("cakes");
-// })
 
 app.listen((process.env.PORT || 5000), function(){
     console.log("Trigo Bakery is running on port 5000");
